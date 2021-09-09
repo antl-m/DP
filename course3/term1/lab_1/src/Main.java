382875bdf1e80ae0769be2a1cf1fb1054877c853
antl-m <tolikmalibroda@gmail.com>
2021-09-09 09:06:30 +0300

    Lab 1 done

---
+++ course3/term1/lab_1/src/Main.java
@@ -0,0 +1,102 @@
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import java.lang.annotation.IncompleteAnnotationException;
+
+class SharedSlider extends JSlider {
+
+    public SharedSlider() {
+        super(0, 100);
+    }
+
+    synchronized public void Increase(int _Increment) {
+        setValue((int)getValue() + _Increment);
+    }
+}
+
+class MyTh extends Thread {
+    private int Increment;
+    private SharedSlider Slider;
+    private int Counter = 0;
+    private static int BOUND = 1000000;
+    private static int THREAD_COUNTER = 0;
+    private int CurrentNumber = 0;
+
+    public MyTh(SharedSlider _Slider, int _Increment, int _Priority) {
+        Slider = _Slider;
+        Increment = _Increment;
+        CurrentNumber = ++THREAD_COUNTER;
+        setPriority(_Priority);
+    }
+
+    @Override
+    public void run() {
+        while (!interrupted()) {
+            int Value = (int)(Slider.getValue());
+            ++Counter;
+            if (Counter > BOUND) {
+                Slider.Increase(Increment);
+                Counter = 0;
+            }
+        }
+    }
+
+    public JPanel GetJPanel() {
+        JPanel Panel = new JPanel();
+        JLabel Label = new JLabel("Thread #" + CurrentNumber + ", Increment: " + Increment);
+        SpinnerModel Model = new SpinnerNumberModel(
+                getPriority(),
+                Thread.MIN_PRIORITY,
+                Thread.MAX_PRIORITY,
+                1
+            );
+        JSpinner Spinner = new JSpinner(Model);
+        Spinner.addChangeListener(e -> {
+                setPriority((int)(Spinner.getValue()));
+            });
+        Panel.add(Label);
+        Panel.add(Spinner);
+
+        return Panel;
+    }
+}
+
+public class Main {
+
+    static Thread th1, th2;
+
+    public static void main(String[] args) {
+        JFrame win = new JFrame();
+        win.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        win.setSize(500, 400);
+
+        SharedSlider Slider = new SharedSlider();
+
+        MyTh Thread1 = new MyTh(Slider, +1, Thread.NORM_PRIORITY);
+        MyTh Thread2 = new MyTh(Slider, -1, Thread.NORM_PRIORITY);
+
+        JButton StartBt = new JButton("Start");
+
+        StartBt.addActionListener(e -> {
+                Thread1.start();
+                Thread2.start();
+                StartBt.setEnabled(false);
+            });
+
+        JPanel MainPanel = new JPanel();
+        MainPanel.setLayout(new BoxLayout(MainPanel, BoxLayout.Y_AXIS));
+
+        MainPanel.add(Thread1.GetJPanel());
+        MainPanel.add(Thread2.GetJPanel());
+        MainPanel.add(Slider);
+
+        JPanel Panel = new JPanel();
+        Panel.add(StartBt);
+        MainPanel.add(Panel);
+
+        win.setContentPane(MainPanel);
+        win.setVisible(true);
+    }
+}
