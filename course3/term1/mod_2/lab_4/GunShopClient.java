947b43f9bceb1786f04551874405f2fa1fee2218
antl-m <tolikmalibroda@gmail.com>
2021-12-09 23:12:08 +0200

    lab 7 done mod 2 labs 1, 2, 3, 4, 5 done

---
+++ course3/term1/mod_2/lab_4/GunShopClient.java
@@ -0,0 +1,43 @@
+package mod_2.lab_4;
+
+import mod_2.lab_2.Knife;
+import mod_2.lab_2.KnifeType;
+import mod_2.lab_4.GunShopRemote;
+
+import java.net.MalformedURLException;
+import java.rmi.Naming;
+import java.rmi.NotBoundException;
+import java.rmi.RemoteException;
+
+public class GunShopClient {
+    public static void main(String[] args) throws RemoteException {
+        GunShopRemote gunShop = null;
+        try {
+            gunShop = (GunShopRemote) Naming.lookup("//" + "localhost" + "/GunShopServer");
+        } catch (NotBoundException | MalformedURLException | RemoteException e) {
+            e.printStackTrace();
+        }
+
+        for(var knifeType : gunShop.getKnifeTypes()) {
+            System.out.println(knifeType);
+
+            for(var knife : gunShop.getKnivesByKnifeType(knifeType.getName())) {
+                System.out.println(knife);
+            }
+        }
+
+        gunShop.addKnifeType(new KnifeType("New KnifeType"));
+        gunShop.addKnife(new Knife("EveryBody", 2.3f, "New KnifeType"));
+        gunShop.updateKnife(new Knife("EveryBody"), new Knife("None", 2.6f, "Comedy"));
+
+        System.out.println("\n");
+
+        for(var knifeType : gunShop.getKnifeTypes()) {
+            System.out.println(knifeType);
+
+            for(var knife : gunShop.getKnivesByKnifeType(knifeType.getName())) {
+                System.out.println(knife);
+            }
+        }
+    }
+}
---
+++ course3/term1/mod_2/lab_4/GunShopRemote.java
@@ -0,0 +1,29 @@
+package mod_2.lab_4;
+
+import mod_2.lab_2.KnifeType;
+import mod_2.lab_2.Knife;
+
+import java.rmi.Remote;
+import java.rmi.RemoteException;
+import java.util.List;
+
+public interface GunShopRemote extends Remote {
+    void addKnifeType(KnifeType knifeType) throws RemoteException;
+
+    void deleteKnifeType(String name) throws RemoteException;
+
+    void addKnife(Knife knife) throws RemoteException;
+
+    void deleteKnife(String name) throws RemoteException;
+
+    void updateKnife(Knife old, Knife other) throws RemoteException;
+
+    int countKnivesByKnifeType(String name) throws RemoteException;
+
+    Knife getKnifeByName(String name) throws RemoteException;
+
+    List<Knife> getKnivesByKnifeType(String name) throws RemoteException;
+
+    List<KnifeType> getKnifeTypes() throws RemoteException;
+}
+
